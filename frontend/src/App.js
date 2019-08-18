94ed4c7283d9bd722ba3c721088ef7c135c52908
Micole Ong <mong16@my.bcit.ca>
2019-08-18 11:53:50 -0700

    update user - final

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,48 @@ import './App.css';
 
 class App extends Component {
   state = {
-    users: []
+    users: [],
+    showUserList: true,
+    isUpdate: false
   }
   
   componentDidMount() {
     this.getUsers();
+  }
+
+  onAddUser = event => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    event.target.name.value = "";
+    this.addUser(name);
+    this.getUsers();
+  } 
+
+  onUpdateUser = event => {
+    event.preventDefault();
+    console.log('Update');
+
+    const oldName = event.target.oldName.value;
+    const newName = event.target.newName.value;
+    event.target.oldName.value = "";
+    event.target.newName.value = "";
+
+    this.updateUser(oldName, newName);
+    this.getUsers();
+  }
+
+  toggleUserList = event => {
+    event.preventDefault();
+    this.setState(prevState => ({
+      showUserList: !prevState.showUserList
+    }))
+  }
+
+  toggleUpdateUser = (event, val) => {
+    event.preventDefault();
+    this.setState({isUpdate: val})
+
+    console.log(this.updateUser);
   }
 
   getUsers = async() =>  {
@@ -20,10 +57,79 @@ class App extends Component {
       })
   }
 
+  addUser = async(name) =>  {
+    await axios.post('/user', {name})
+      .then(res => {
+        console.log(res);
+      })
+  }
+
+  updateUser = async(oldName, newName) => {
+    await axios.put('/user', {oldName, newName})
+      .then(res => {
+        console.log(res);
+      })
+  }
+
   render() {
     return (
-        <Users users={this.state.users} />
-      );
+        <div>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#" onClick={(e) => this.toggleUpdateUser(e, false)}>Add User <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#" onClick={(e) => this.toggleUpdateUser(e, true)}>Update User</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#" onClick={this.toggleUserList}>User List</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          {!this.state.isUpdate && (
+          <form onSubmit={this.onAddUser}>
+            <div className="form-row align-items-center">
+              <div className="col-sm-3 my-1">
+                <label className="sr-only">Name</label>
+                <input type="text" className="form-control" name="name" placeholder="Name" />
+              </div>        
+              <div className="col-auto my-1">
+                <button type="submit" className="btn btn-primary">Add User</button>
+              </div>     
+            </div>
+          </form>
+          )}
+
+          {this.state.isUpdate && (
+            <form onSubmit={this.onUpdateUser}>
+            <div className="form-row">
+              <div className="form-group col-md-3">
+                <label>Old Name</label>
+                <input type="text" className="form-control" name="oldName" placeholder="Old Name" />
+              </div>
+            </div>
+
+            <div className="form-row">
+              <div className="form-group col-md-3">
+                <label>New Name</label>
+                <input type="text" className="form-control" name="newName" placeholder="New Name" />
+              </div>
+            </div>
+            <button className="btn btn-primary" type="submit">Update User</button>
+          </form>
+          )}
+          
+          {!this.state.showUserList && (
+            <Users users={this.state.users} />
+          )}
+          
+        </div>
+        
+    );
   }
   
 }
--- frontend/src/components/users.js
+++ frontend/src/components/users.js
@@ -3,22 +3,15 @@ import React from 'react';
 const Users = ({ users }) => {
     return (
         <div>
-            <div className="input-group">
-                <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-                <div className="input-group-append">
-                    <input className="btn btn-outline-secondary" type="button" value="Button" />
-                    <input className="btn btn-outline-secondary" type="button" value="Button2" />
-                </div>
-            </div>
+            
             <div className="list-group">
                 <center><h1>User List</h1></center>
-                {users && users.map((user) => (
-                    <div className="card">
-                        <div className="card-body">
-                            <h6 className="card-title">{user.name}</h6>
-                        </div>
-                    </div>
-                ))}
+                <ul className="list-group">
+                    {users && users.map((user) => (
+                        
+                        <li className="list-group-item">{user.name}</li>
+                    ))}
+                </ul>
             </div>
         </div>
     )
